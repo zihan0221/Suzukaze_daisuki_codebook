f0dd44d28537b2ee94b68980e496e891eb1832ae
revival0728 <revival0728@gmail.com>
2024-09-13 00:33:43 +0800

    update

--- basic/py_trivial.py
+++ basic/py_trivial.py
@@ -1,7 +1,9 @@
-a = list(map(int,input().split()))
+import sys, os
+
+# 設定大數運算最大位數, 複雜度需考慮運算位數
+sys.set_int_max_str_digits(100000)
 
 # 開讀檔
-import sys, os.path
 if(os.path.exists('input_file.txt')):
     sys.stdin = open("input_file.txt","r")
     sys.stdout = open("output_file.txt","w")
--- math/matrix_fast_pow.cpp
+++ math/matrix_fast_pow.cpp
@@ -1,29 +1,18 @@
-LL len,mod;
-vector<vector<LL>> operator*(vector<vector<LL>> x,vector<vector<LL>> y){
-    vector<vector<LL>> ret(len,vector<LL>(len,0));
-    for(int i=0;i<len;i++){
-        for(int j=0;j<len;j++){
-            for(int k=0;k<len;k++){
-                ret[i][j]=(ret[i][j]+x[i][k]*y[k][j])%mod;
-    }   }   }
-    return ret;
+template<class T> using Matrix = std::vector<std::vector<T>>;
+template<class T> void set_matrix(Matrix<T>& m, int x, int y, T v) {
+  m = Matrix<T>(x, std::vector<T>(y, v));
 }
-struct Martix_fast_pow{ //O(len^3 lg k)
-    LL init(int _len,LL m=9223372036854775783LL){
-        len=_len, mod=m;
-    }   // mfp.solve(k,{0, 1}, {1, 1}) k'th fib {值,係數} // 0-base
-    LL solve(LL n,vector<vector<LL>> poly){
-        if(n<len)   return poly[n][0];
-        vector<vector<LL>> mar(len,vector<LL>(len,0)),x(len,vector<LL>(len,0));
-        for(int i=0;i<len;i++)    mar[i][i]=1;
-        for(int i=0;i+1<len;i++)  x[i][i+1]=1;
-        for(int i=0;i<len;i++)    x[len-1][i]=poly[i][1];
-        while(n){
-            if(n&1) mar=mar*x;
-            n>>=1, x=x*x;
-        }
-        LL ans=0;
-        for(int i=0;i<len;i++)   ans=(ans+mar[len-1][i]*poly[i][0]%mod)%mod;
-        return ans;
+template<class T> Matrix<T> operator*(const Matrix<T>& a, const Matrix<T>& b) {
+  assert(a[0].size() == b.size());
+  int sa = a.size(), sb = b[0].size(), sc = a[0].size();
+  Matrix<T> ret; set_matrix(ret, sa, sb, 0);
+  for(int i = 0; i < sa; ++i) {
+    for(int j = 0; j < sb; ++j) {
+      for(int k = 0; k < sc; ++k) {
+        ret[i][j] += a[i][k] * b[k][j];
+        // add ret[i][j] %= MOD if needed
+      }
     }
-}mfp;
+  }
+  return ret;
+}
