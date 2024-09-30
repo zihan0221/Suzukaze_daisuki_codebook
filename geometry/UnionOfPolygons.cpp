4ac363ad37a87258ff644927a97f3301f9c98b8d
revival0728 <revival0728@gmail.com>
2024-09-30 12:20:21 +0800

    updated pt_declare and fixed PolyUnion

--- geometry/UnionOfPolygons.cpp
+++ geometry/UnionOfPolygons.cpp
@@ -1,48 +1,77 @@
-// Area[i] : area covered by at least i polygon
-vector<double> PolyUnion( vector<vector<Pt>> &P) {
-    int n = P.size();
-    vector<double> Area(n + 1);//Area[i] 為至少包括 i 個多邊形的覆蓋面積
-    vector<Line> Ls;
-    for (int i = 0; i < n; i++) 
-        for (int j = 0; j < P[i].size(); j++)
-            Ls.push_back({P[i][j], P[i][(j + 1) % P[i].size()]});
-    auto cmp = [&](Line &l, Line &r) {
-        Pt u = l.b - l.a, v = r.b - r.a;
-        if (argcmp(u, v)) return true;
-        if (argcmp(v, u)) return false;
-        return PtSide(l.a, r) < 0;
-    };
-    sort(all(Ls), cmp);
-    for (int l = 0, r = 0; l < Ls.size(); l = r) {
-        while (r < Ls.size() and !cmp(Ls[l], Ls[r])) r++;
-        Line L = Ls[l];
-        vector<pair<Pt, int>> event;
-        for (auto [c, d] : Ls) {
-            if (sgn((L.a - L.b) ^ (c - d)) != 0) {
-                int s1 = PtSide(c, L) == 1;
-                int s2 = PtSide(d, L) == 1;
-                if (s1 ^ s2) event.emplace_back(LineInter(L, {c, d}), s1 ? 1 : -1);
-            } else if (PtSide(c, L) == 0 and sgn((L.a - L.b) * (c - d)) > 0) {
-                event.emplace_back(c, 2);
-                event.emplace_back(d, -2);
-            }
-        }
-        sort(all(event), [&](auto i, auto j) {
-            return (L.a - i.ff) * (L.a - L.b) < (L.a - j.ff) * (L.a - L.b);
-        });
-        int cov = 0, tag = 0;
-        Pt lst{0, 0};
-        for (auto [p, s] : event) {
-            if (cov >= tag) {
-                Area[cov] += lst ^ p;
-                Area[cov - tag] -= lst ^ p;
-            }
-            if (abs(s) == 1) cov += s;
-            else tag += s / 2;
-            lst = p;
-        }
+// Need Line && norm()
+double ori(Pt a, Pt b, Pt c) { return (b - a) ^ (c - a); }
+int PtSide(Pt p, Line L) {
+  return dcmp(ori(L.s, L.e, p) / norm(L.s - L.e));
+}
+bool argcmp(const Pt &a, const Pt &b) { // arg(a) < arg(b)
+  int f = (Pt{a.y, -a.x} > Pt{} ? 1 : -1) * (a != Pt{});
+  int g = (Pt{b.y, -b.x} > Pt{} ? 1 : -1) * (b != Pt{});
+  // int f = (Pt{a.y, -a.x} > Pt{} ? 1 : -1) * (a != Pt{});
+  // int g = (Pt{b.y, -b.x} > Pt{} ? 1 : -1) * (b != Pt{});
+  return f == g ? (a ^ b) > 0 : f < g;
+}
+Pt LineInter(Line l, Line m) {
+  double s = ori(m.s, m.e, l.s), t = ori(m.s, m.e, l.e);
+  return (l.e * s - l.s * t) / (s - t);
+}
+#ifndef all
+  #define all(x) (x).begin(), (x).end()
+#endif
+vector < double > PolyUnion(vector < vector < Pt >> & P) {
+  int n = P.size();
+  vector < double > Area(n + 1); //Area[i] 為至少包括 i 個多邊形的覆蓋面積
+  vector < Line > Ls;
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < P[i].size(); j++)
+      Ls.push_back({
+        P[i][j],
+        P[i][(j + 1) % P[i].size()]
+      });
+  auto cmp = [ & ](Line & l, Line & r) {
+    Pt u = l.v, v = l.v;
+    if (argcmp(u, v)) return true;
+    if (argcmp(v, u)) return false;
+    return PtSide(l.s, r) < 0;
+  };
+  sort(all(Ls), cmp);
+  for (int l = 0, r = 0; l < Ls.size(); l = r) {
+    while (r < Ls.size() and!cmp(Ls[l], Ls[r])) r++;
+    Line L = Ls[l];
+    vector < pair < Pt, int >> event;
+    for (auto& ls : Ls) {
+      auto c = ls.s, d = ls.e;
+      if (dcmp((L.s - L.e) ^ (c - d)) != 0) {
+        int s1 = PtSide(c, L) == 1;
+        int s2 = PtSide(d, L) == 1;
+        if (s1 ^ s2) event.emplace_back(LineInter(L, {
+          c,
+          d
+        }), s1 ? 1 : -1);
+      } else if (PtSide(c, L) == 0 and dcmp((L.s - L.e) * (c - d)) > 0) {
+        event.emplace_back(c, 2);
+        event.emplace_back(d, -2);
+      }
     }
-    for (int i = n - 1; i >= 0; i--) Area[i] += Area[i + 1];
-    for (int i = 1; i <= n; i++) Area[i] /= 2;
-    return Area;
+    sort(all(event), [ & ](auto i, auto j) {
+      return (L.s - i.first) * (L.s - L.e) < (L.s - j.second) * (L.s - L.e);
+    });
+    int cov = 0, tag = 0;
+    Pt lst {
+      0,
+      0
+    };
+    for (auto[p, s]: event) {
+      if (cov >= tag) {
+        Area[cov] += lst ^ p;
+        Area[cov - tag] -= lst ^ p;
+      }
+      if (abs(s) == 1) cov += s;
+      else tag += s / 2;
+      lst = p;
+    }
+  }
+  for (int i = n - 1; i >= 0; i--) Area[i] += Area[i + 1];
+  for (int i = 1; i <= n; i++) Area[i] /= 2;
+  return Area;
 };
+#undef all
